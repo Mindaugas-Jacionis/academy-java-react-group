902ed6ac3c6db946bea9f4eadad2489431c5305f
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-06-17 18:28:07 +0300

    Initial CRA setup + Logo & Link components

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo from './components/Logo';
+import Link from './components/Link';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Link href="https://google.com">Linko Textas</Link>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World
         </a>
       </header>
     </div>
