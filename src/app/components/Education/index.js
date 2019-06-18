6b91dae656d521589ec7e590e0741fc7ea4e7834
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-06-18 21:35:20 +0300

    Adds basic Skills CV section

--- src/app/components/Education/index.js
+++ src/app/components/Education/index.js
@@ -3,17 +3,17 @@ import './index.css';
 
 function Item(data) {
   return (
-    <div>
-      <h5>{`Degree: ${data.degree}`}</h5>
-      <p>{data.university}</p>
-      <p>{data.year}</p>
+    <div className="Education--item">
+      <h5 className="Education--item-degree">{`Degree: ${data.degree}`}</h5>
+      <p className="Education--item-university">{data.university}</p>
+      <p className="Education--item-year">{data.year}</p>
     </div>
   );
 }
 
 function Education(props) {
   return (
-    <section>
+    <section className="Education">
       <h3>Education</h3>
       {props.items.map(Item)}
     </section>
--- src/app/index.js
+++ src/app/index.js
@@ -4,8 +4,19 @@ import { FaGithubAlt, FaEnvelope, FaPhone, FaMapMarker, FaLinkedinIn } from 'rea
 import Header from './components/Header';
 import Contacts from './components/Contacts';
 import Education from './components/Education';
+import Skills from './components/Skills';
 import './App.css';
 
+const skills = [
+  { text: 'Sleeping', level: 'advanced' },
+  { text: 'Running', level: 'beginner' },
+  { text: 'Coding', level: 'intermediate' },
+  { text: 'Speaking', level: 'intermediate' },
+  { text: 'Listening', level: 'beginner' },
+  { text: 'Driving', level: 'beginner' },
+  { text: 'Eating', level: 'advanced' },
+  { text: 'MS Word', level: 'beginner' },
+];
 const education = [
   {
     degree: 'PHD',
@@ -49,6 +60,7 @@ function App() {
         <div className="App--content-left">
           <Contacts items={contacts} />
           <Education items={education} />
+          <Skills items={skills} />
         </div>
         <div className="App--content-right">Main content</div>
       </main>
